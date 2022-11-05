4f8665d33034c6d768834b439a0ee93c3ca4521f
DragonMort <90557202+DragonMortYT@users.noreply.github.com>
2022-11-05 21:51:54 +0100

    inital commits

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+
+    token:"MTAzNzc4MDEwNjM5MjkxNjAzOA.GOnSg9.Pu_gnnn7XDQwryyeW2CHhTuLUCJnb4KB6dA5cA"
+}
---
+++ main.js
@@ -0,0 +1,6 @@
+const Discord = require("discord.js")
+const bot = new Discord.Client({Intents: 3276799})
+const config = require("./config")
+const kick = require("./commands/kick")
+
+client.login(process.env.TOKEN)
