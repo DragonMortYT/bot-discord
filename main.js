9f41b710c64ca6889db181260f4453d88eecfb68
DragonMort <90557202+DragonMortYT@users.noreply.github.com>
2022-11-05 21:45:24 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,5 +3,4 @@ const bot = new Discord.Client({Intents: 3276799})
 const config = require("./config")
 const kick = require("./commands/kick")
 
-bot.login(config.token)
 Client.login(process.env.TOKEN)
