dea1c22715c4efb73042854eb8b18988e64f1f4f
DragonMort <90557202+DragonMortYT@users.noreply.github.com>
2022-11-19 22:59:28 +0100

    Add files via upload

---
+++ commands/ping.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { CommandInteraction } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Renvoie le nombre de ping'),
+    /**
+     * 
+     * @param {CommandInteraction} interaction 
+     */
+    async execute(interaction) {
+        await interaction.reply(`Ton ping est de ${interaction.client.ws.ping} ms.`);
+    }
+}
