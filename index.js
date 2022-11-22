d75c7024b04ffbc4ad088bbf553f10b8c0b4c5cf
DragonMort <90557202+DragonMortYT@users.noreply.github.com>
2022-11-22 13:16:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
 const roleClaim = require('./utils/role-claim');
 
-const client = new Client({ intents: [GatewayIntentsBits.FLAGS.GUILDS, GatewayIntentsBits.FLAGS.GUILD_MESSAGE_REACTIONS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
 
 client.once('ready', () => {
     console.log('Ready!');
